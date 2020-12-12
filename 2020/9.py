c81a7f0e720709844fbda0d4566a4f5e623eab6b
Antoine Almoussa <antoinealmoussa@antoine.almoussa>
2020-12-12 19:18:31 +0100

    Day 10 11 12 2020

--- 2020/9.py
+++ 2020/9.py
@@ -1,14 +1,16 @@
 from time import time
 from itertools import combinations
+from collections import deque
 import unittest
 a = time()
+
 
 def readFile():
     # On renvoie le fichier d'entrée sous forme d'une liste de string
     with open(__file__[:-3] + "-" + "input.txt", "r") as f:
         return [int(i.strip()) for i in f.readlines()]
 
-            
+
 def part1(scheme: list):
     ind = 0
     while ind < len(scheme)-25:
@@ -16,6 +18,7 @@ def part1(scheme: list):
             return scheme[ind+25]
         else:
             ind += 1
+        
 
 def part2(scheme: list):
     res = part1(scheme)
@@ -31,6 +34,7 @@ def part2(scheme: list):
             partialInd += 1
         startInd += 1
 
+
 class Tests(unittest.TestCase):
 
     def testP1(self):
@@ -45,4 +49,3 @@ if __name__ == "__main__":
     p2 = part2(scheme)
     print(f"Part 2: {p2}")
     print(time()-a)
-
